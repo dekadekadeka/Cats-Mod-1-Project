a89ce335635d4d88ab2c93355b817baddb536da8
ascotttoney <ascotttoney@gmail.com>
2019-05-07 10:47:04 -0400

    Updated migrate files.

--- db/migrate/002_create_neighborhood.rb
+++
@@ -1,4 +0,0 @@
-class CreateNeighborghood < ActiveRecord::Migrate[5.2]
-    def change
-    end
-end
---
+++ db/migrate/002_create_neighborhoods.rb
@@ -0,0 +1,8 @@
+class CreateNeighborghoods < ActiveRecord::Migrate
+    def change
+        create_table :neighborhoods do |t|
+            t.string :name
+            t.string :location #city and state
+        end
+    end
+end
