c4a935963f19d740a8ad1d6ea22d70015fffae9d
ascotttoney <ascotttoney@gmail.com>
2019-05-07 16:10:21 -0400

    Updated schema to correct temperament

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "cats", force: :cascade do |t|
     t.string  "name"
     t.string  "breed"
     t.string  "color"
     t.string  "favorite_food"
-    t.boolean "temperment"
+    t.boolean "temperament"
   end
 
   create_table "neighborhoods", force: :cascade do |t|
