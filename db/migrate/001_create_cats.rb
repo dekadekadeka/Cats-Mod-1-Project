e88424f5e1aa27823b2690d299f5cb8f41a91408
ascotttoney <ascotttoney@gmail.com>
2019-05-08 10:50:23 -0400

    Changes to migrate, seed, and database.

--- db/migrate/001_create_cats.rb
+++ db/migrate/001_create_cats.rb
@@ -6,6 +6,8 @@ class CreateCats < ActiveRecord::Migration[5.0]
             t.string :color
             t.string :favorite_food  # wet or dry only
             t.boolean :temperament  # gets along with other cats? true or false
+            t.integer :neighborhood_id
+            t.integer :owner_id
         end
     end
 end
--- db/migrate/005_update_tables.rb
+++
@@ -1,6 +0,0 @@
-class UpdateTables < ActiveRecord::Migration[5.0]
-    def change
-        add_column :cats, :neighborhood, :string
-        add_column :owners, :neighborhood, :string 
-    end
-end
