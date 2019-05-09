4d1fee1e67f6afeb2415b25a94717ae400eab0ee
github username <dekadekadeka@gmail.com>
2019-05-09 15:45:55 -0400

    fix run

--- bin/run.rb
+++ bin/run.rb
@@ -2,8 +2,9 @@ require_relative '../config/environment'
 
 ActiveRecord::Base.logger = nil
 
-# pid = fork{ exec 'afplay', 'cat_scratch_fever.mp3' }
+fork{ exec 'afplay', 'cat_scratch_fever.mp3' }
 
 # Title.new.start
-
+fork{exec 'lolcat -a -d 5 lib/titlesequence.plaintext'}
+sleep 9
 DirectoryController.new.start
