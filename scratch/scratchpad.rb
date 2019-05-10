148c14744a2fd7e146cf7f8bd9d8829a411f170f
ascotttoney <ascotttoney@gmail.com>
2019-05-10 12:12:23 -0400

    few fixes

--- scratch/scratchpad.rb
+++ scratch/scratchpad.rb
@@ -6,3 +6,19 @@
 
 # lemon = Cat.find_by(name: "Lemon")
 # paul = Cat.find_by(name: "Paul")
+
+
+def edit_cat_info
+    puts "\nPlease enter your cat's name and then press the RETURN key."
+        kitty_name = gets.chomp
+        kitty = Cat.all.find_by(name: kitty_name)
+
+
+
+puts "\nPlease enter your cat's name and then press the RETURN key."
+kitty_name = gets.chomp
+kitty = Cat.all.find_by(name: kitty_name)
+    if kitty.nil?
+        puts "\nThat meowzer doesn't yet exist. You'll now be transferred to the ADD CAT menu."
+        add_cat
+    end
